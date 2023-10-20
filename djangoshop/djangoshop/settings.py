5fd1348c894f70999f30f047c5725e1c4b3c60df
Mary <zemlyanikasweet@mail.ru>
2023-10-20 17:22:22 +0300

    Django_shop_bd_

--- djangoshop/djangoshop/settings.py
+++ djangoshop/djangoshop/settings.py
@@ -76,8 +76,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'djangoshop.wsgi.application'
 
-Database
-https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
